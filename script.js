76bebe99f151eaa98b182abf6e751cdd50dfb133
André Duarte <42449246+andreduarte99@users.noreply.github.com>
2024-12-09 12:46:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const iconeComecarOuPausar = document.querySelector('.app__card-primary-butto-ic
 const tempoNaTela = document.querySelector('#timer');
 
 const musicaFocoInput = document.querySelector('#alternar-musica');
-const musica = new Audio('/sons/luna-rise-part-one.mp3');
-const beep = new Audio('/sons/beep.mp3');
-const play = new Audio('/sons/play.wav');
-const pause = new Audio('/sons/pause.mp3');
+const musica = new Audio('./sons/luna-rise-part-one.mp3');
+const beep = new Audio('./sons/beep.mp3');
+const play = new Audio('./sons/play.wav');
+const pause = new Audio('./sons/pause.mp3');
 
 let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
@@ -56,7 +56,7 @@ function alterarContexto(contexto){
         contexto.classList.remove('active');
     });
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
