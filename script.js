b2776e633f5b8609cf7a5953304aa002045de47e
André Duarte <140335@corp.hrln.br>
2024-12-12 14:12:13 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const beep = new Audio('./sons/beep.mp3');
 const play = new Audio('./sons/play.wav');
 const pause = new Audio('./sons/pause.mp3');
 
-let tempoDecorridoEmSegundos = 1500;
+let tempoDecorridoEmSegundos = 15;
 let intervaloId = null;
 
 musica.loop = true;
@@ -31,19 +31,19 @@ musicaFocoInput.addEventListener('change', () => {
 
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500;
+    tempoDecorridoEmSegundos = 15;
     alterarContexto('foco');
     focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 300;
+    tempoDecorridoEmSegundos = 3;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 900;
+    tempoDecorridoEmSegundos = 9;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 });
@@ -84,6 +84,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
         beep.play();
         alert('Tempo finalizado!');
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if(focoAtivo){
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento);
+        };
         zerar(); 
         return 
     }
