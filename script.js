a5b08203c9d58fe5c1cae4b41917bb69d1556d52
André Duarte <42449246+andreduarte99@users.noreply.github.com>
2024-12-15 14:55:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const beep = new Audio('./sons/beep.mp3');
 const play = new Audio('./sons/play.wav');
 const pause = new Audio('./sons/pause.mp3');
 
-let tempoDecorridoEmSegundos = 15;
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
@@ -31,19 +31,19 @@ musicaFocoInput.addEventListener('change', () => {
 
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 15;
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco');
     focoBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 3;
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 9;
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 });
